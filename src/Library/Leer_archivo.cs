7ba0c03ea98edb173e3a2ec1d98feabadb39594f
Rodrigo-Doldan <rodrigodoldan04@gmail.com>
2023-08-30 19:52:27 -0300

    Edited classes

--- src/Library/Leer_archivo.cs
+++ src/Library/Leer_archivo.cs
@@ -8,5 +8,18 @@ namespace conway.equipo20;
 
 public class Leer_archivo
 {
-
+    string url = "ConwayGame\src\Library\archivo.txt";
+    string content = File.ReadAllText(url);
+    string[] contentLines = content.Split('\n');
+    bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
+    for (int  y=0; y<contentLines.Length;y++)
+        {
+            for (int x=0; x<contentLines[y].Length; x++)
+                {
+                    if(contentLines[y][x]=='1')
+                {
+                board[x,y]=true;
+                }
+        }
+}
 }
