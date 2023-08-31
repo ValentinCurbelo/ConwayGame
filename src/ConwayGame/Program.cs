c28acc6ec5814552c08ff928c00b5b9c57b3ee27
Rodrigo-Doldan <rodrigodoldan04@gmail.com>
2023-08-30 21:08:49 -0300

    Quick fix

--- src/ConwayGame/Program.cs
+++ src/ConwayGame/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Conwaygame
 {
-   public class Program
+    public class Program
     {
-       public static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            string filePath = @"C:\Users\rodri\OneDrive\Escritorio\Coding\Visual studio\Programacion 2 c#\ConwayGame\src\Library\archivo.txt"; 
+            string filePath = @"C:\Users\rodri\OneDrive\Escritorio\Coding\Visual studio\Programacion 2 c#\ConwayGame\src\Library\archivo.txt"; // La ruta relativa no funciona, copiar ruta exacta.
             bool[,] gameBoard = LeerArchivo.LeerTablero(filePath);
 
             while (true)
